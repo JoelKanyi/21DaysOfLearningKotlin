39accc4b4279bc1b8bbe2e5cc3d665886b18c1b9
JoelKanyi <joelkanyi98@gmail.com>
2020-05-17 20:38:50 +0300

    some revision on kotlin extensions

---
+++ src/KtExtensions.kt
@@ -0,0 +1,44 @@
+
+fun main() {
+    val str1:String = "Hello "
+    val str2:String = "World"
+    val str3:String = "Hey "
+
+    val num1:Int = 5
+    val num2:Int = 4
+    val greaterVal = num1.greaterValue(num2)
+    println(greaterVal)
+
+    val student = Student()
+
+    fun Student.isScholar(grade:Int) : Boolean{
+        if(grade>60)
+            print("scholar")
+        else
+            print("not a scholar")
+        return true
+    }
+
+
+
+    println(str3.add(str1,str2))
+    println()
+
+}
+class Student{
+    fun hasPassed(marks:Int): Boolean{
+        return marks>60
+    }
+}
+
+//extension function to concatenate three strings
+fun String.add(s1:String,s2:String): String{
+    return this + s1 + s2;
+}
+
+//extension function to find the large number between the given two
+fun Int.greaterValue(otherNum:Int): Int{
+    if(this > otherNum) return this
+    else return otherNum;
+}
+
