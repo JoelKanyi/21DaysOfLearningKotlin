5687ba348d7dcb5dcea17c388f79586085e67217
JoelKanyi <joelkanyi98@gmail.com>
2020-05-17 20:53:25 +0300

    renamed:    src/IfElse.kt -> src/controlstructures.kt

--- src/controlstructures.kt
+++ src/controlstructures.kt
@@ -2,13 +2,13 @@ fun main() {
     var age = 18
 
     //if...else
-    /*if(age<18)
+   if(age<18)
         print("Not elegible for voting")
     else
-        print("Eligible for voting")*/
+        print("Eligible for voting")
 
     //if... else-if
-    /*if (age<18)
+    if (age<18)
         print("Not eligible for voting")
     else if (age>18 && age<45){
         print("Eligible for voting")
@@ -16,11 +16,10 @@ fun main() {
     else if(age>45 && age<65)
         print("About to retire")
     else
-        print("Not categorised")*/
+        print("Not categorised")
 
 
     //when
-
     when(age){
         in 0..17 -> print("Not Eligible for Voting")
         in 18..45 -> print("Eligle for voting")
@@ -28,7 +27,4 @@ fun main() {
             print("Not categorised")
         }
     }
-
-
-
 }
