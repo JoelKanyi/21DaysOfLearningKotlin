4032a1003fce69deb321debf527b591eb5674614
JoelKanyi <joelkanyi98@gmail.com>
2020-05-17 20:55:29 +0300

    modified:   src/Looping.kt

--- src/Looping.kt
+++ src/Looping.kt
@@ -1,22 +1,20 @@
 fun main() {
     //for loop
-    /*for(i in 1..10)
-        print(i)*/
+    for(i in 1..10)
+        print(i)
 
     //while loop
-    /*var i = 0;
+    var i = 0;
     while(i<=10) {
         print(i)
         i += 1
-    }*/
+    }
 
     //do while loop
-   /* var i = 0
+   var i = 0
     do {
         print(i)
         i+=1
     }
-        while (i<=10)*/
-
-
+        while (i<=10)
 }
